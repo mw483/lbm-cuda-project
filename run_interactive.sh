d87cd3cb95bda552f7b64f126d45f339cbc433c4
mw483 <wijaya.m.ab@m.titech.ac.jp>
2026-07-08 10:05:55 +0900

    run interactive node bug fix

--- run_interactive.sh
+++ run_interactive.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-hours_runtime = "00"
-minutes_runtime = "30"
-seconds_runtime = "00"
+hours_runtime="00"
+minutes_runtime="30"
+seconds_runtime="00"
 
-group = "tga-lbmcity"
+group="tga-lbmcity"
 
 
 iqrsh -g $group -l h_rt=$hours_runtime:$minutes_runtime:$seconds_runtime
